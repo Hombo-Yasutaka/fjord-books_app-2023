60cef67d080fd267d7b284614c614d4126c6bf0c
Hombo-Yasutaka <y.honbo@genbasupport.com>
2023-11-20 05:05:00 +0000

    自分のコメントのみ削除できるようにする

--- app/controllers/base/comments_controller.rb
+++ app/controllers/base/comments_controller.rb
@@ -12,7 +12,7 @@ class Base::CommentsController < ApplicationController
   private
 
   def set_comment
-    @comment = Comment.find(params[:id])
+    @comment = current_user.comments.find(params[:id])
   end
 
   def comment_params
